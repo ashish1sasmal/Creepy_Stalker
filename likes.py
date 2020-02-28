f110d7ee7c3cdd1f07ccd511b15238a71f4eb47f
ashish1sasmal <ashishsasmal1@gmail.com>
2020-02-28 22:54:59 +0530

    first commit

---
+++ likes.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
+
+browser = webdriver.Chrome(executable_path='./chromedriver')
+
+
+browser.get('http://www.facebook.com')
+elem = browser.find_element_by_name('email')
+
+elem.send_keys(os.environ['FB_ID'])
+
+elem = browser.find_element_by_name('pass')
+
+elem.send_keys(os.environ['FB_PASS'])
+
+# h=['2','b','4']
+# for i in h:
+# 		elem = browser.find_element_by_xpath('//*[@id="u_0_'+i+'"]')
+# 		elem.click()
+
+elem = browser.find_element_by_xpath('//*[@id="u_0_b"]')
+elem.click()
